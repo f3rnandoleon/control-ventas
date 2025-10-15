f9b31877a3ec6816cb13c6647835f5a65633da60
fernando leon <ferleonqaq@gmail.com>
2025-10-15 02:21:57 -0400

    inicio

---
+++ src/libs/mongodb.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const {MONGODB_URL } =process.env;
+if(!MONGODB_URL){
+    throw new Error("MONGO DB debe estar definido");
+}
+export const connectDB= async()=>{
+    try{
+        const {connection}=await mongoose.connect(MONGODB_URL);
+        if(connection.readyState==1){
+            return Promise.resolve(true);
+        }
+    }catch(error){
+        return Promise.resolve(false);
+        
+    }
+    mongoose.connect('')
+}
